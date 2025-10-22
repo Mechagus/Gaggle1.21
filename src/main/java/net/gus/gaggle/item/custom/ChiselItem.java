70b5a8f42e677cf1a69a3901005529833cdfe157
Mechagus <angus.greif@gmail.com>
2025-10-22 08:09:30 -0700

    S'more

---
+++ src/main/java/net/gus/gaggle/item/custom/ChiselItem.java
@@ -0,0 +1,10 @@
+package net.gus.gaggle.item.custom;
+
+import net.minecraft.item.Item;
+
+public class ChiselItem extends Item {
+    public ChiselItem(Settings settings) {
+        super(settings);
+    }
+
+}
